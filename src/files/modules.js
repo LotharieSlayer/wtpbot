5c1af1148abb1cfd263dd49849e9b1d986cabd2e
Lotharie <49253492+LotharieSlayer@users.noreply.github.com>
2021-11-29 22:54:30 +0100

    0.0.2
    - Modules.js to authorize execution of some utils.

---
+++ src/files/modules.js
@@ -0,0 +1,16 @@
+/**
+ * @author Lothaire Guée
+ * @description
+ * 		The file contains authorisation for module to execute.
+ */
+
+/**
+ * Export if yes or no the module is authorize to execute
+ * @export {boolean} Authorisation.
+ */
+
+exports.Certify = true;
+exports.Proposition = true;
+exports.Presentation = true;
+exports.WelcomeRole = true;
+exports.ActiveMember = true;
