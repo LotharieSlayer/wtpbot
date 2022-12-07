a29228da86658a9e6adc03f4cf84a83753a6b8e4
Lotharie <49253492+LotharieSlayer@users.noreply.github.com>
2022-12-08 00:18:39 +0100

    Plugin Example

---
+++ src/plugins/example/files/modules.js
@@ -0,0 +1,12 @@
+/**
+ * @author Lothaire Guée
+ * @description
+ * 		The file contains authorisation for module to execute.
+ */
+
+/**
+ * Export if yes or no the module is authorize to execute
+ * @export {boolean} Authorisation.
+ */
+
+exports.ExampleModule4Plugin = true;
